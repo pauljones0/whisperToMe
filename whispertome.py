b4a9557f4174e1ec2a869c5b6223c278ff4c02ac
Paul Jones <bethekind@gmail.com>
2024-02-19 20:08:33 -0600

    Cleaned up, refactored code, reducing code duplication and un-necessary checks/exceptions, added more doc-strings and comments.

--- whispertome.py
+++ whispertome.py
@@ -1,6 +1,7 @@
 import pathlib
 from redbot.core import commands
 import discord
+import tempfile
 from openai import OpenAI
 import os
 import json
@@ -14,102 +15,115 @@ class WhisperToMe(commands.Cog):
         self.bot = bot
         self.language_validation_list = self._load_validation_language_dict()
         self.listening = False
+        self._load_env_vars()
         self.client = self._load_open_ai_client()
-        self.lang_code = self._load_lang_code()
 
     def _load_validation_language_dict(self):
         with open('ISO-639-1-language.json', 'r') as f:
             languages = json.load(f)
         return {language['code']: language['name'] for language in languages}
 
-    def create_env_if_needed_and_load(self):
-        if not os.path.isfile('.env'):
-            with open('.env', 'w') as f:
-                pass
+    def _load_env_vars(self):
+        """Load environment variables."""
         load_dotenv()
-
-    def _load_lang_code(self):
-        self.create_env_if_needed_and_load()
-        lang_code = os.getenv("ISO_639_1_LANGUAGE_CODE")
-        if lang_code is None:
-            with open('.env', 'a') as f:
-                f.write(f'ISO_639_1_LANGUAGE_CODE={lang_code}\n')
-            load_dotenv()
-        return lang_code
+        self.api_key = os.getenv("OPENAI_API_KEY", '')
+        self.lang_code = os.getenv("ISO_639_1_LANGUAGE_CODE", 'en')
 
     def _load_open_ai_client(self):
-        self.create_env_if_needed_and_load()
-        api_key = os.getenv("OPENAI_API_KEY")
-        if api_key is None:
-            with open('.env', 'a') as f:
-                f.write(f'OPENAI_API_KEY={api_key}\n')
-            load_dotenv()
-            return api_key
+        if not self.api_key:
+            raise ValueError("OPENAI_API_KEY is not set in the environment variables.")
         else:
-            return OpenAI(api_key=api_key)
+            return OpenAI(api_key=self.api_key)
 
     async def _validate_api_key(self, ctx, client):
+        """Validate the API key and provide detailed error messages."""
         try:
-            temporary_client = client
-            if temporary_client is None:
-                await ctx.send("No API key set in the environment variables.")
-                return False
-            else:
-                models = temporary_client.models.list().data
-                if any(model.id == "whisper-1" for model in models):
-                    await ctx.send("API key is valid...")
-                    return True
-                else:
-                    await ctx.send(
-                        "API key is valid, but does not have access to the whisper-1 model.\nModify the restrictions "
-                        "on this key, in order to use it.")
-                    return False
+            models = client.models.list().data
         except Exception as e:
-            await ctx.send(f"An unexpected error occurred:\n{e}")
+            await ctx.send(f"An error occurred while validating the API key:\n{e}")
+            return False
+        if any(model.id == "whisper-1" for model in models):
+            await ctx.send("API key is valid...")
+            return True
+        else:
+            await ctx.send(
+                "API key is valid, but does not have access to the whisper-1 model.\nModify the restrictions "
+                "on this key, in order to use it.")
             return False
 
     async def _validate_lang_code(self, ctx, lang_code):
-        if lang_code is None:
-            await ctx.send(f"No language code set in the environment variables.")
-            return False
-        elif lang_code in self.language_validation_list:
+        if lang_code in self.language_validation_list:
             return True
         else:
             await ctx.send(f"Invalid language code {lang_code} in the environment variables.")
             return False
 
+    async def _validate_env_vars(self, ctx):
+        """Validate environment variables and provide detailed error messages."""
+        valid_lang_code = await self._validate_lang_code(ctx, self.lang_code)
+        valid_api_key = await self._validate_api_key(ctx, self.client)
+        if not valid_lang_code:
+            await ctx.send("Language code is not set or is invalid.\nPlease set a valid language code before calling "
+                           "`!start` again.")
+            return False
+        if not valid_api_key:
+            await ctx.send("API key is not set or is invalid.\nPlease set a valid API key before calling `!start` "
+                           "again.")
+            return False
+        return True
+
+    async def _should_ignore_message(self, message):
+        return not self.listening or message.author.bot or not message.flags.voice
+
+    async def _process_voice_message(self, message):
+        try:
+            with tempfile.NamedTemporaryFile(suffix=".ogg", delete=True) as temp_file:
+                # voice messages only have a single attachment, containing the ogg file
+                await message.attachments[0].save(fp=pathlib.WindowsPath(tempfile.gettempdir()+temp_file.name))
+                transcript = self._transcribe_voice_message(temp_file)
+                await message.reply(transcript.text)
+        except Exception as e:
+            await message.channel.send(f"Error processing voice message:\n{str(e)}")
+
+    def _transcribe_voice_message(self, temp_file):
+        with open(temp_file.name, "rb") as file:
+            return self.client.audio.transcriptions.create(file=file, model="whisper-1",
+                                                           language=self.lang_code,
+                                                           response_format="json")
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if await self._should_ignore_message(message):
+            return
+
+        await self._process_voice_message(message)
+
+    async def _check_listening_status(self, ctx, status):
+        if self.listening == status:
+            await ctx.send(f"I'm already {'listening' if status else 'not listening'} to messages.\nCommand ignored.")
+            return True
+        return False
+
+    async def _toggle_listening(self, status, ctx):
+        self.listening = status
+        status_string = "Started" if status else "Stopped"
+        await ctx.send(f"{status_string} listening to all messages.")
+
     @commands.admin()
     @commands.command()
     async def start(self, ctx):
         """Start listening to incoming voice messages"""
-        valid_lang_code = await self._validate_lang_code(ctx, self.lang_code)
-        if valid_lang_code:
-            valid_api_key = await self._validate_api_key(ctx, self.client)
-            if valid_api_key:
-                await ctx.send("Both environment variables are set and valid.")
-            else:
-                await ctx.send("API key is not set or is invalid.\nPlease set a valid API key before calling `!start` "
-                               "again.")
-                return
-        else:
-            await ctx.send("Language code is not set or is invalid.\nPlease set a valid language code before calling "
-                           "`!start` again.")
+        if not await self._validate_env_vars(ctx) or await self._check_listening_status(ctx, True):
             return
-        if self.listening:
-            await ctx.send("I'm already listening to messages.\nStart command ignored.")
-        else:
-            self.listening = True
-            await ctx.send(f"Started listening to all messages.")
+        await self._toggle_listening(True, ctx)
 
     @commands.admin()
     @commands.command()
     async def stop(self, ctx):
         """Stop listening to incoming voice messages"""
-        if self.listening:
-            self.listening = False
-            await ctx.send("Stopped listening to messages.")
-        else:
-            await ctx.send(f"Wasn't listening to messages.\nStop command ignored.")
+        if await self._check_listening_status(ctx, False):
+            return
+        await self._toggle_listening(False, ctx)
 
     @commands.admin()
     @commands.command()
@@ -130,33 +144,9 @@ class WhisperToMe(commands.Cog):
         """Set the OpenAI API key to use for transcriptions."""
         valid_api_key = await self._validate_api_key(ctx, OpenAI(api_key=key))
         if valid_api_key:
-            # os.putenv only works for the current process, so we need to use set_key from the dotenv library
             set_key(".env", "OPENAI_API_KEY", key)
             self.client = OpenAI(api_key=key)
             await ctx.send("API key set.\nUse the `!start` command to begin listening!")
         else:
             await ctx.send("API key not set due to errors.\nPlease try again.")
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        deafened = not self.listening
-        is_a_bot = message.author.bot
-        is_not_a_voice_message = not message.flags.voice
-
-        if deafened or is_a_bot or is_not_a_voice_message:
-            return  # Don't transcribe if it's a message from a bot, not a voice message or if the bot is deafened
-        for attachment in message.attachments:
-            if attachment.is_voice_message():
-                try:
-                    #using attachment.bytes should be possible, I've checked and it's an ogg file, but openai api rejects it, so using save, use, delete file for now
-                    await attachment.save(fp=pathlib.WindowsPath("./voice_message.ogg"))
-                    with open("voice_message.ogg", "rb") as file:
-                        transcript = self.client.audio.transcriptions.create(file=file, model="whisper-1",
-                                                                             language=self.lang_code,
-                                                                             response_format="json")
-                    await message.reply(transcript.text)
-                except Exception as e:
-                    await message.channel.send(f"Error processing voice message:\n{str(e)}")
-                finally:
-                    if os.path.exists("voice_message.ogg"):
-                        os.remove("voice_message.ogg")
